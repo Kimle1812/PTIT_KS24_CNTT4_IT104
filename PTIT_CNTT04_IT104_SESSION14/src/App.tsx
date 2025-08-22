6aa4279b52f02fa6af1cd1e4074fb7e2347be126
Kimle1812 <nguyenthikimle18122003@gmail.com>
2025-08-22 09:35:16 +0700

    up code

--- PTIT_CNTT04_IT104_SESSION14/src/App.tsx
+++ PTIT_CNTT04_IT104_SESSION14/src/App.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Controll from './components/Controll';
-import Exercise05 from './components/PTIT_CNTT04_IT104_SESSION14_EXERCISE05/exercise05';
+import Exercise05 from './components/PTIT_CNTT04_IT104_SESSION14_EXERCISE05/Exercise05';
 import Exercise06 from './components/PTIT_CNTT04_IT104_SESSION14_EXERCISE06/Exercise06';
+import Register from './components/PTIT_CNTT04_IT104_SESSION14_EXERCISE07/Register';
+import Login from './components/PTIT_CNTT04_IT104_SESSION14_EXERCISE08/Login';
 
 
 export default class App extends Component<{}> {
@@ -25,10 +27,13 @@ constructor(props:{}){
         {/* Bài 5 */}
         {/* <Exercise05></Exercise05> */}
         {/* Bài 6 */}
-        <Exercise06></Exercise06>
+        {/* <Exercise06></Exercise06> */}
         {/* Bài 7 */}
+        {/* <Register></Register> */}
         {/* Bài 8 */}
+        {/* <Login></Login> */}
         {/* Bài 9 */}
+        
         {/* Bài 10 */}
       </div>
     )
